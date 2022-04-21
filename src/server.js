e0be6972aeef428783aa447deaf4df566fabcdda
mateocampillo <mateocampillo1@gmail.com>
2022-04-21 04:40:27 -0300

    fuente, listo para empezar

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static("/public"));
+app.use(express.static("public"));
 
 app.listen(3000, () => { console.log("Servidor corriendo en el puerto 3000") });
 
