7678ba1960016ec9a243dd5942b00ae47556e777
mateocampillo <mateocampillo1@gmail.com>
2022-04-21 04:27:35 -0300

    Estructura principal funcional

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static("/public"));
+
+app.listen(3000, () => { console.log("Servidor corriendo en el puerto 3000") });
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "/views/index.html"))
+});
